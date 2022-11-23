0c4cc2c2ea3ce3799f393bc02b735893cfea0722
JoepLiefting <joep.liefting@gmail.com>
2022-11-23 16:26:30 +0100

    revert vehicles

--- time_window_vehicle.py
+++ time_window_vehicle.py
@@ -1,10 +1,12 @@
 import read_files
 
-def time_window_barge(vehicle_id, type='str'):
-    if vehicle_id in read_files.vehicles.index:
-        Ap = read_files.vehicles['Ap']
-    return Ap
+# def time_window_barge(vehicle_id, type='str'):
+#     for vehicle_id in read_files.vehicles['K']:
+#         Ap = read_files.vehicles['Ap']
+#     return Ap
+#
+# time_window_barge('Barge1')
+#
+# print(time_window_barge('Barge1'))
 
-time_window_barge('Barge1')
-
-print(time_window_barge('Barge1'))
+print(read_files.vehicles.item("Barge1"))
