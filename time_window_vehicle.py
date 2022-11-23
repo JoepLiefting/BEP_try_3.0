8e8369c6353a1d327c2bf1619cc2f31a72fddf88
JoepLiefting <joep.liefting@gmail.com>
2022-11-23 15:36:13 +0100

    different files

--- time_window_vehicle.py
+++ time_window_vehicle.py
@@ -1,2 +1,10 @@
-def time_window_barge(vehicle_id)
-    for vehicle_id in read_files.
+import read_files
+
+def time_window_barge(vehicle_id, type='str'):
+    if vehicle_id in read_files.vehicles.index:
+        Ap = read_files.vehicles['Ap']
+    return Ap
+
+time_window_barge('Barge1')
+
+print(time_window_barge('Barge1'))
