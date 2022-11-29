faad2ca7fe619d2ed6466cb5a674a44eb0dda16b
JoepLiefting <joep.liefting@gmail.com>
2022-11-29 12:46:27 +0100

    time_windows_2 matrix werkend

--- time_windows_2.py
+++ time_windows_2.py
@@ -7,31 +7,31 @@ requests = read_files.R
 time_window_check = np.zeros([len(vehicles), len(requests)])
 
 
-for vehicle in vehicles:
-    for request in requests:
-        vehicle_id = vehicle[0]
-        vehicle_ap = vehicle[10]
-        vehicle_bp = vehicle[11]
-        vehicle_ad = vehicle[12]
-        vehicle_bd = vehicle[13]
-        vehicle_type = vehicle[7]
-        vehicle_origin = vehicle[8]
-        vehicle_destination = vehicle[9]
+for v in range(len(vehicles)):
+    for r in range(len(requests)):
+        vehicle_id = vehicles[v][0]
+        vehicle_ap = vehicles[v][10]
+        vehicle_bp = vehicles[v][11]
+        vehicle_ad = vehicles[v][12]
+        vehicle_bd = vehicles[v][13]
+        vehicle_type = vehicles[v][7]
+        vehicle_origin = vehicles[v][8]
+        vehicle_destination = vehicles[v][9]
 
-        request_id = request[7]
-        request_ap = request[2]
-        request_bp = request[3]
-        request_ad = request[4]
-        request_bd = request[5]
-        request_origin = request[0]
-        request_destination = request[1]
+        request_id = requests[r][7]
+        request_ap = requests[r][2]
+        request_bp = requests[r][3]
+        request_ad = requests[r][4]
+        request_bd = requests[r][5]
+        request_origin = requests[r][0]
+        request_destination = requests[r][1]
 
         if vehicle_type == 1:
             if vehicle_bp > request_ap and vehicle_ad < request_bd:
 
                 print(f"{request_id}" " in " f"{vehicle_id}")
+                time_window_check[v][r] = 1
 
-                time_window_check[vehicle_id][request_id] = 1
 
 
 
