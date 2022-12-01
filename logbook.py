de32d13246d5f28bff805ade4f5a23f1ba414419
JoepLiefting <joep.liefting@gmail.com>
2022-12-01 15:56:11 +0100

    direct routes barge assigned

--- logbook.py
+++ logbook.py
@@ -1,10 +1,23 @@
 import numpy as np
-import read_files
+import capacity_vehicles
 
-vehicles = read_files.vehicles
+def assigned_requests_matrix(vehicles, requests):
+    vehicles = vehicles
+    requests = requests
+    assigned_requests = np.zeros([len(vehicles), len(requests)])
+    return assigned_requests
 
-
-def assign_request_to_vehicle(requests, capacities, vehicle_id, request_id):
+def assign_request_to_vehicle(requests, capacities, assignments, assigned_requests):
 
     requests = requests
-    capacity_check =
+    capacities = capacities
+    assigned_requests = assigned_requests
+    assignment_vehicle = assignments[0][0]
+    assignment_request = assignments[1][0]
+
+    capacity_vehicles.update_curcap(requests= requests,
+                                  capacities= capacities,
+                                  request_id= assignment_request+100000,
+                                  vehicle_id= assignment_vehicle)
+
+    assigned_requests[assignment_vehicle][assignment_request] = 1
--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import read_files
 import time_windows_3
 import OD_matrices
 import capacity_vehicles
+import logbook
 
 requests = read_files.R
+open_requests = requests
 vehicles = read_files.vehicles
 capacities = capacity_vehicles.capacity_matrix(vehicles= vehicles)
+assigned_requests = logbook.assigned_requests_matrix(vehicles= vehicles, requests= requests)
 
 time_window_barge = time_windows_3.time_matrix_barge(requests= requests)
 time_window_train = time_windows_3.time_matrix_train(requests= requests)
@@ -22,12 +25,11 @@ capacity_check = capacity_vehicles.capacity_check(requests= requests, capacities
 suitable_routes_barge = capacity_check * time_window_barge * ((O_matrix_barge * D_matrix_barge) + O_matrix_barge)
 suitable_routes_train = capacity_check * time_window_train * ((O_matrix_train * D_matrix_train) + O_matrix_train)
 
+direct_routes_barge = np.where(suitable_routes_barge == 2)
 
-update_capacity = capacity_vehicles.update_curcap(requests= requests,
-                                                  capacities= capacities,
-                                                  vehicle_id= 2,
-                                                  request_id= 100001)
+assign_direct_routes_barge = logbook.assign_request_to_vehicle(requests= requests,
+                                                               capacities= capacities,
+                                                               assignments= direct_routes_barge,
+                                                               assigned_requests= assigned_requests)
 
-direct_routes_barge_tuple = np.where(suitable_routes_barge == 2)
 
-# assign_direct_routes_barge =
