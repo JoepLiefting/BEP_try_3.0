c3079a2f3b23e89ab3776f3e5598089c92c6f4f0
JoepLiefting <joep.liefting@gmail.com>
2022-12-09 17:54:31 +0100

    T_E_matrix barge en train werkend

--- OD_matrices.py
+++ OD_matrices.py
@@ -91,44 +91,72 @@ def D_matrix_train(requests):
 
 
 def available_routes_matrix(E_matrix, vehicles, time_window_matrix, request_id):
-    available_vehicles = []
-    T_E_matrix = E_matrix
+    available_barges = []
+    available_trains = []
+    T_E_matrix = E_matrix.copy()
 
-
+# T_E_matrix voor barges
     for v in range(len(vehicles)):
-        if time_window_matrix[v][request_id] == 1:
-            available_vehicles.append(v)
+        if vehicles[v][7] == 1:
+            if time_window_matrix[v][request_id] == 1:
+                available_barges.append(v)
 
-    origins = []
-    destinations = []
+    barge_origins = []
+    barge_destinations = []
 
-    for v in range(len(available_vehicles)):
-        vehicle_id = available_vehicles[v]
-        vehicle_O = vehicles[vehicle_id][8]
-        vehicle_D = vehicles[vehicle_id][9]
-        origins.append(int(vehicle_O))
-        destinations.append(int(vehicle_D))
+    for v in range(len(available_barges)):
+        vehicle_id = available_barges[v]
+        barge_O = vehicles[vehicle_id][8]
+        barge_D = vehicles[vehicle_id][9]
+        barge_origins.append(int(barge_O))
+        barge_destinations.append(int(barge_D))
 
-    print(E_matrix[0][3])
-    for i in range(0, 9):
-        for j in range(0, 9):
+    for i in range(9):
+        for j in range(9):
             if i != j:
                 T_E_matrix[i][j] = 0
 
-    print(E_matrix[0][3])
+    for i in range(len(barge_origins)):
+        if len(barge_origins) != 0:
+            barge_origin = int(barge_origins[i])
+            barge_destination = int(barge_destinations[i])
+            T_E_matrix[barge_origin][barge_destination] = E_matrix[barge_origin][barge_destination]
+            T_E_matrix[barge_destination][barge_origin] = E_matrix[barge_destination][barge_origin]
 
-    for i in range(len(origins)):
-        if len(origins) != 0:
-            origin = int(origins[i])
-            destination = int(destinations[i])
-            T_E_matrix[origin][destination] = E_matrix[origin][destination]
+# T_E_matrix voor trains
+        for v in range(len(vehicles)):
+            if vehicles[v][7] == 2:
+                if time_window_matrix[v][request_id] == 1:
+                    available_trains.append(v)
+
+        train_origins = []
+        train_destinations = []
+
+        for v in range(len(available_trains)):
+            vehicle_id = available_trains[v]
+            train_O = vehicles[vehicle_id][8]
+            train_D = vehicles[vehicle_id][9]
+            train_origins.append(int(train_O))
+            train_destinations.append(int(train_D))
+
+        for i in range(9):
+            for j in range(9):
+                if i != j:
+                    T_E_matrix[i + 10][j + 10] = 0
+
+        for i in range(len(train_origins)):
+            if len(train_origins) != 0:
+                train_origin = int(train_origins[i])
+                train_destination = int(train_destinations[i])
+                T_E_matrix[train_origin][train_destination] = E_matrix[train_origin][train_destination]
+                T_E_matrix[train_destination][train_origin] = E_matrix[train_destination][train_origin]
 
     return T_E_matrix
 
 E_matrix = read_files.E_matrix_All
 vehicles = read_files.vehicles
 requests = read_files.R
-time_window_matrix = time_windows_3.time_matrix_barge(requests= requests)
+time_window_matrix = time_windows_3.time_matrix_barge(requests= requests)+time_windows_3.time_matrix_train(requests= requests)
 
 
 T_E_matrix = available_routes_matrix(E_matrix= E_matrix,
