76a14562836179e10004c9d1d6e0835164fb265d
JoepLiefting <joep.liefting@gmail.com>
2022-12-11 17:18:06 +0100

    TEmatrix barge en train werkend met capacities

--- OD_matrices.py
+++ OD_matrices.py
@@ -1,6 +1,7 @@
 import read_files
 import numpy as np
 import time_windows_3
+import capacity_vehicles
 
 def O_matrix_barge(requests):
 
@@ -90,7 +91,7 @@ def D_matrix_train(requests):
     return D_check_train
 
 
-def available_routes_matrix(E_matrix, vehicles, time_window_matrix, request_id):
+def available_routes_matrix(E_matrix, vehicles, time_window_matrix, request_id, capacity_check):
     available_barges = []
     available_trains = []
     T_E_matrix = E_matrix.copy()
@@ -98,7 +99,7 @@ def available_routes_matrix(E_matrix, vehicles, time_window_matrix, request_id):
 # T_E_matrix voor barges
     for v in range(len(vehicles)):
         if vehicles[v][7] == 1:
-            if time_window_matrix[v][request_id] == 1:
+            if time_window_matrix[v][request_id] == 1 and capacity_check[v][request_id] == 1:
                 available_barges.append(v)
 
     barge_origins = []
@@ -124,43 +125,49 @@ def available_routes_matrix(E_matrix, vehicles, time_window_matrix, request_id):
             T_E_matrix[barge_destination][barge_origin] = E_matrix[barge_destination][barge_origin]
 
 # T_E_matrix voor trains
-        for v in range(len(vehicles)):
-            if vehicles[v][7] == 2:
-                if time_window_matrix[v][request_id] == 1:
-                    available_trains.append(v)
+    for v in range(len(vehicles)):
+        if vehicles[v][7] == 2:
+            if time_window_matrix[v][request_id] == 1 and capacity_check[v][request_id] == 1:
+                available_trains.append(v)
 
-        train_origins = []
-        train_destinations = []
+    train_origins = []
+    train_destinations = []
 
-        for v in range(len(available_trains)):
-            vehicle_id = available_trains[v]
-            train_O = vehicles[vehicle_id][8]
-            train_D = vehicles[vehicle_id][9]
-            train_origins.append(int(train_O))
-            train_destinations.append(int(train_D))
+    for v in range(len(available_trains)):
+        vehicle_id = available_trains[v]
+        train_O = vehicles[vehicle_id][8]
+        train_D = vehicles[vehicle_id][9]
+        train_origins.append(int(train_O))
+        train_destinations.append(int(train_D))
 
-        for i in range(9):
-            for j in range(9):
-                if i != j:
-                    T_E_matrix[i + 10][j + 10] = 0
+    for i in range(9):
+        for j in range(9):
+            if i != j:
+                T_E_matrix[i + 10][j + 10] = 0
 
-        for i in range(len(train_origins)):
-            if len(train_origins) != 0:
-                train_origin = int(train_origins[i])
-                train_destination = int(train_destinations[i])
-                T_E_matrix[train_origin][train_destination] = E_matrix[train_origin][train_destination]
-                T_E_matrix[train_destination][train_origin] = E_matrix[train_destination][train_origin]
+    for i in range(len(train_origins)):
+        if len(train_origins) != 0:
+            train_origin = int(train_origins[i])
+            train_destination = int(train_destinations[i])
+            T_E_matrix[train_origin][train_destination] = E_matrix[train_origin][train_destination]
+            T_E_matrix[train_destination][train_origin] = E_matrix[train_destination][train_origin]
 
     return T_E_matrix
+
+
+
+
 
 E_matrix = read_files.E_matrix_All
 vehicles = read_files.vehicles
 requests = read_files.R
 time_window_matrix = time_windows_3.time_matrix_barge(requests= requests)+time_windows_3.time_matrix_train(requests= requests)
-
+capacities = capacity_vehicles.capacity_matrix(vehicles= vehicles)
+capacity_check = capacity_vehicles.capacity_check(requests= requests, capacities= capacities, vehicles= vehicles)
 
 T_E_matrix = available_routes_matrix(E_matrix= E_matrix,
                                      vehicles= vehicles,
                                      time_window_matrix= time_window_matrix,
-                                     request_id= 4)
+                                     request_id= 4,
+                                     capacity_check= capacity_check)
 
