2d86c6323f1bcdc0d01e88ef3dd5a0288acd446b
JoepLiefting <joep.liefting@gmail.com>
2022-12-14 14:50:09 +0100

    assignments werkt volledig

--- logbook.py
+++ logbook.py
@@ -1,18 +1,20 @@
 import numpy as np
 import capacity_vehicles
 
+
 def assigned_requests_matrix(vehicles, requests):
     vehicles = vehicles
     requests = requests
     assigned_requests = np.zeros([len(vehicles), len(requests)])
     return assigned_requests
 
+
 def assignment_lowest_capacity(vehicles, capacities, assignments):
     unique_assignments = assignments.copy()
 
-    for v in range(len(assignments[0])-1):
-        vehicle = assignments[0][v+1]
-        request = assignments[1][v+1]
+    for v in range(len(assignments[0]) - 1):
+        vehicle = assignments[0][v + 1]
+        request = assignments[1][v + 1]
         previous_vehicle = assignments[0][v]
         previous_request = assignments[1][v]
 
@@ -27,25 +29,26 @@ def assignment_lowest_capacity(vehicles, capacities, assignments):
             if vehicle_curcap >= previous_vehicle_curcap:
                 unique_assignments = np.delete(unique_assignments, v, 1)
             else:
-                unique_assignments = np.delete(unique_assignments, v+1, 1)
+                unique_assignments = np.delete(unique_assignments, v + 1, 1)
 
     return unique_assignments
 
 
-def assign_request_to_vehicle(open_requests, closed_requests, vehicles, capacities, vehicle_id, request_id, assigned_requests):
-
+def assign_request_to_vehicle(open_requests, closed_requests, vehicles, capacities, vehicle_id, request_id,
+                              assigned_requests):
     assignment_request = request_id
     assignment_vehicle = vehicle_id
-    capacity_check = capacity_vehicles.capacity_check(requests= open_requests, capacities= capacities, vehicles= vehicles)
+    capacity_check = capacity_vehicles.capacity_check(requests=open_requests, capacities=capacities, vehicles=vehicles)
 
-    if capacity_check[assignment_vehicle][assignment_request] == 1 and assigned_requests[assignment_vehicle][assignment_request] != 1 and assignment_request not in closed_requests:
-
-        capacity_vehicles.update_curcap(requests= open_requests,
-                                      capacities= capacities,
-                                      request_id= assignment_request+100000,
-                                      vehicle_id= assignment_vehicle)
+    if capacity_check[assignment_vehicle][assignment_request] == 1 and assigned_requests[assignment_vehicle][
+        assignment_request] != 1 and assignment_request not in closed_requests:
+        capacity_vehicles.update_curcap(requests=open_requests,
+                                        capacities=capacities,
+                                        request_id=assignment_request + 100000,
+                                        vehicle_id=assignment_vehicle)
 
         assigned_requests[assignment_vehicle][assignment_request] = 1
-        closed_requests.append(assignment_request)
 
-    return assigned_requests, closed_requests
+    return assigned_requests
+
+
--- main.py
+++ main.py
@@ -13,45 +13,50 @@ requests = read_files.R
 open_requests = requests.copy()
 closed_requests = []
 vehicles = read_files.vehicles
-capacities = capacity_vehicles.capacity_matrix(vehicles= vehicles)
-assigned_requests = logbook.assigned_requests_matrix(vehicles= vehicles, requests= requests)
+capacities = capacity_vehicles.capacity_matrix(vehicles=vehicles)
+assigned_requests = logbook.assigned_requests_matrix(vehicles=vehicles, requests=requests)
 E_matrix_All = read_files.E_matrix_All
 H_matrix = read_files.H_matrix
 
 # Decision matrices for selecting possible vehicles
-time_window_barge = time_windows_3.time_matrix_barge(requests= requests)
-time_window_train = time_windows_3.time_matrix_train(requests= requests)
-time_window_combined = time_window_train+time_window_barge
-O_matrix_barge = OD_matrices.O_matrix_barge(requests= requests)
-D_matrix_barge = OD_matrices.D_matrix_barge(requests= requests)
-O_matrix_train = OD_matrices.O_matrix_train(requests= requests)
-D_matrix_train = OD_matrices.D_matrix_train(requests= requests)
-capacity_check = capacity_vehicles.capacity_check(requests= requests, capacities= capacities, vehicles= vehicles)
+time_window_barge = time_windows_3.time_matrix_barge(requests=requests)
+time_window_train = time_windows_3.time_matrix_train(requests=requests)
+time_window_combined = time_window_train + time_window_barge
+O_matrix_barge = OD_matrices.O_matrix_barge(requests=requests)
+D_matrix_barge = OD_matrices.D_matrix_barge(requests=requests)
+O_matrix_train = OD_matrices.O_matrix_train(requests=requests)
+D_matrix_train = OD_matrices.D_matrix_train(requests=requests)
+capacity_check = capacity_vehicles.capacity_check(requests=requests, capacities=capacities, vehicles=vehicles)
 
 # Compute decision values for each request based on time_window, fixed_vehicle_type and available capacity
 direct_routes_barge_check = capacity_check * time_window_barge * ((O_matrix_barge * D_matrix_barge) + O_matrix_barge)
 direct_routes_train_check = capacity_check * time_window_train * ((O_matrix_train * D_matrix_train) + O_matrix_train)
 
-#Assigning direct requests to barges
+# Assigning direct requests to barges
 for r in range(len(requests)):
     for v in range(len(vehicles)):
-        direct_routes_barge_check = capacity_check * time_window_barge * ((O_matrix_barge * D_matrix_barge) + O_matrix_barge)
+        direct_routes_barge_check = capacity_check * time_window_barge * (
+                (O_matrix_barge * D_matrix_barge) + O_matrix_barge)
 
-        #Possible direct routes barge
+        # Possible direct routes barge
         if direct_routes_barge_check[v][r] == 2:
-
             # Assign requests to vehicles
-            assign_direct_routes_barge = logbook.assign_request_to_vehicle(open_requests= open_requests,
-                                                                           closed_requests= closed_requests,
-                                                                           vehicles= vehicles,
-                                                                           capacities= capacities,
-                                                                           vehicle_id= v,
-                                                                           request_id= r,
-                                                                           assigned_requests= assigned_requests)
-#Assigning direct requests to trains
+            assign_direct_routes_barge = logbook.assign_request_to_vehicle(open_requests=open_requests,
+                                                                           closed_requests=closed_requests,
+                                                                           vehicles=vehicles,
+                                                                           capacities=capacities,
+                                                                           vehicle_id=v,
+                                                                           request_id=r,
+                                                                           assigned_requests=assigned_requests)
+
+            if assigned_requests[v][r] == 1 and r not in closed_requests:
+                closed_requests.append(r)
+
+# Assigning direct requests to trains
 for r in range(len(requests)):
     for v in range(len(vehicles)):
-        direct_routes_train_check = capacity_check * time_window_barge * ((O_matrix_barge * D_matrix_barge) + O_matrix_barge)
+        direct_routes_train_check = capacity_check * time_window_barge * (
+                (O_matrix_barge * D_matrix_barge) + O_matrix_barge)
 
         # Possible direct routes train
         if direct_routes_train_check[v][r] == 2:
@@ -63,36 +68,39 @@ for r in range(len(requests)):
                                                                            vehicle_id=v,
                                                                            request_id=r,
                                                                            assigned_requests=assigned_requests)
+            if assigned_requests[v][r] == 1 and r not in closed_requests:
+                closed_requests.append(r)
 
-#Astar requests:
+# Astar requests:
 for r in range(len(requests)):
-    request_id = requests[r][7]-100000
+    request_id = requests[r][7] - 100000
     used_vehicles = []
     if request_id not in closed_requests:
-    # if r == 4:
-        capacity_check = capacity_vehicles.capacity_check(requests= requests, capacities= capacities, vehicles= vehicles)
-        CTE_matrix = OD_matrices.CTE_matrix(E_matrix= E_matrix_All,
-                                            vehicles= vehicles,
-                                            time_window_matrix= time_window_combined,
-                                            request_id= r,
-                                            capacity_check= capacity_check)
+        capacity_check = capacity_vehicles.capacity_check(requests=requests, capacities=capacities, vehicles=vehicles)
+        CTE_matrix = OD_matrices.CTE_matrix(E_matrix=E_matrix_All,
+                                            vehicles=vehicles,
+                                            time_window_matrix=time_window_combined,
+                                            request_id=r,
+                                            capacity_check=capacity_check)
 
-        traject = A_star.a_star(graph= CTE_matrix,
-                               heuristic= H_matrix,
-                               start= requests[r][0],
-                               goal= requests[r][1])
+        traject = A_star.a_star(graph=CTE_matrix,
+                                heuristic=H_matrix,
+                                start=requests[r][0],
+                                goal=requests[r][1])
 
-        unique_used_vehicles = A_star.get_vehicles_from_astar(traject= traject,
-                                                    vehicles= vehicles,
-                                                    request_id= r,
-                                                    time_window_matrix= time_window_combined)
-        print(unique_used_vehicles)
+        unique_used_vehicles = A_star.get_vehicles_from_astar(traject=traject,
+                                                              vehicles=vehicles,
+                                                              request_id=r,
+                                                              time_window_matrix=time_window_combined)
+        # print(unique_used_vehicles)
         for v in range(len(unique_used_vehicles)):
             vehicle_id = int(unique_used_vehicles[v])
-            assign_astar_routes = logbook.assign_request_to_vehicle(open_requests= open_requests,
-                                                                closed_requests= closed_requests,
-                                                                vehicles= vehicles,
-                                                                capacities= capacities,
-                                                                vehicle_id= vehicle_id,
-                                                                request_id= request_id,
-                                                                assigned_requests= assigned_requests)
+            assign_astar_routes = logbook.assign_request_to_vehicle(open_requests=open_requests,
+                                                                    closed_requests=closed_requests,
+                                                                    vehicles=vehicles,
+                                                                    capacities=capacities,
+                                                                    vehicle_id=vehicle_id,
+                                                                    request_id=request_id,
+                                                                    assigned_requests=assigned_requests)
+
+        closed_requests.append(request_id)
